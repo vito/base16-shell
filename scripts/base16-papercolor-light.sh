8962a5076f0a5383923100d1dbe15f3b35d7980c
Alex Suraci <suraci.alex@gmail.com>
2021-06-20 14:04:31 -0400

    rebuild
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- scripts/base16-papercolor-light.sh
+++ scripts/base16-papercolor-light.sh
@@ -10,7 +10,7 @@ color03="d7/00/87" # Base 0A - Yellow
 color04="d7/5f/00" # Base 0D - Blue
 color05="00/5f/af" # Base 0E - Magenta
 color06="d7/5f/00" # Base 0C - Cyan
-color07="87/87/87" # Base 05 - White
+color07="44/44/44" # Base 05 - White
 color08="5f/87/00" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
 color10=$color02 # Base 0B - Bright Green
@@ -18,14 +18,14 @@ color11=$color03 # Base 0A - Bright Yellow
 color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
-color15="44/44/44" # Base 07 - Bright White
+color15="87/87/87" # Base 07 - Bright White
 color16="d7/00/00" # Base 09
 color17="00/5f/87" # Base 0F
 color18="af/00/00" # Base 01
 color19="00/87/00" # Base 02
 color20="00/87/af" # Base 04
 color21="00/5f/87" # Base 06
-color_foreground="87/87/87" # Base 05
+color_foreground="44/44/44" # Base 05
 color_background="ee/ee/ee" # Base 00
 
 if [ -n "$TMUX" ]; then
@@ -78,12 +78,12 @@ put_template 21 $color21
 # foreground / background / cursor color
 if [ -n "$ITERM_SESSION_ID" ]; then
   # iTerm2 proprietary escape codes
-  put_template_custom Pg 878787 # foreground
+  put_template_custom Pg 444444 # foreground
   put_template_custom Ph eeeeee # background
-  put_template_custom Pi 878787 # bold color
+  put_template_custom Pi 444444 # bold color
   put_template_custom Pj 008700 # selection color
-  put_template_custom Pk 878787 # selected text color
-  put_template_custom Pl 878787 # cursor
+  put_template_custom Pk 444444 # selected text color
+  put_template_custom Pl 444444 # cursor
   put_template_custom Pm eeeeee # cursor text
 else
   put_template_var 10 $color_foreground
--- scripts/base16-snazzy.sh
+++ scripts/base16-snazzy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # base16-shell (https://github.com/chriskempson/base16-shell)
 # Base16 Shell template by Chris Kempson (http://chriskempson.com)
-# Snazzy scheme by Chawye Hsu (https://github.com/h404bi) based on Hyper Snazzy Theme (https://github.com/sindresorhus/hyper-snazzy)
+# Snazzy scheme by Chawye Hsu (https://github.com/chawyehsu) based on Hyper Snazzy Theme (https://github.com/sindresorhus/hyper-snazzy)
 
 color00="28/2a/36" # Base 00 - Black
 color01="ff/5c/57" # Base 08 - Red
